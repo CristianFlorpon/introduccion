9a56bf00618656e516959cc15198be10b89d0468
CristianFlorpon <159298448+CristianFlorpon@users.noreply.github.com>
2024-02-07 13:56:23 -0400

    Add files via upload

---
+++ mayorde3numeros.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	float a,b,c;
+		
+	cout << "Ingrese el primer numero porfavor";
+	cin >> a;
+	cout << "Ingrese el segundo numero porfavor";
+	cin >> b;
+	cout << "Ingrese el ultimo numero porfavor";
+	cin >> c;
+	if(a>b && a>c){
+		cout<<"el numero mayor es: "<<a;
+	}
+	if(b>c && b>a){
+		cout<<"el numero mayor es: "<<b;        
+	}
+	if(c>b && c>a){
+		cout<<"el numero mayor es: "<<c;        
+	}
+	
+	cout << "El numero mayor es: " << c << endl;
+	return 0;
+}
+
