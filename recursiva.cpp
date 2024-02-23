92dacbe5ccd481306d41e8b40416b490915dae9b
CristianFlorpon <159298448+CristianFlorpon@users.noreply.github.com>
2024-02-23 13:38:58 -0400

    Add files via upload

---
+++ recursiva.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void imprimir (int * n, int cant)
+{
+	for (int i = 0; i<cant; i++)
+		cout<<i<<" = "<<n[i]<<endl;
+}
+void imprec (int * n,int cant){
+	if (cant>0)
+	{
+		imprec(n,cant-1);
+		cout<<cant<<" = "<<n<<n[cant-1]<<endl;
+	}
+}
+int main(int argc, char *argv[]) {
+	const int Tam = 7;
+	int numeros [] =  {3,5,2,0,1,7,4};
+	cout<<"IMPRESION BUCLE "<<endl;
+	imprimir (numeros,Tam);
+	cout<<"IMPRESION RECURSIVA "<<endl;
+	imprec (numeros,Tam);
+	return 0;
+}
+
