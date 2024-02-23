1dec9abaf7f8a2e06ad179ae75fe8e7caba1ce10
CristianFlorpon <159298448+CristianFlorpon@users.noreply.github.com>
2024-02-23 13:37:31 -0400

    Add files via upload

---
+++ ejercicio de puntero.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void funcion (int var){
+	var =100;
+}
+int main(int argc, char *argv[]) {
+	
+	int var=10;
+	int num=0;
+	double real=10
+	double *ptreal;
+	int * puntero= NULL;
+	int * ptreal= NULL;
+	puntero=&var;
+	puntero=&num;
+	ptreal=puntero;
+	
+	cout<<"var: "<<var<<endl;
+	cout<<"Valor puntero: "<<*puntero<<endl;
+	cout<<"Direccion de puntero: "<<puntero<<endl;
+	return 0;
+}
+
