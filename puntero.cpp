e0e11216068fa8ea29117a9b0d262693f5602eaa
CristianFlorpon <159298448+CristianFlorpon@users.noreply.github.com>
2024-02-23 13:37:04 -0400

    Add files via upload

---
+++ puntero.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int var=10;
+	int * puntero;
+	puntero=&var;
+	*puntero=100;
+	cout<<"var"<<var<<endl;
+	cout<<"puntero : "<<*puntero<<endl;
+	return 0;
+}
+
